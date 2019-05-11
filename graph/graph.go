55f8dee8b25c2f6ff3f1713e27b3d006b2a86830
luthierycosta <luthierycosta2010@gmail.com>
2019-05-11 17:20:52 -0300

    Trocando tabs por spaces; brincando mais um pouco

--- graph/graph.go
+++ graph/graph.go
@@ -1,66 +1,69 @@
 package graph
 
 import (
-	"fmt"
+    "fmt"
 )
 
 type Graph struct {
-	Vertices []*Vertex
+    Vertices []*Vertex
 }
 
 type Vertex struct {
-	Value int
-	Adjacents []*Vertex
+    Value int
+    Adjacents []*Vertex
 }
 
 func NewGraph(vertices ...int) *Graph {
-	g := Graph{}
-	for i := range vertices {
-		g.AddVertex(vertices[i])
-	}
-	return &g
+    g := new(Graph)
+    for i := range vertices {
+        g.AddVertex(vertices[i])
+    }
+    return g
 }
+
 func NewVertex(v int) *Vertex {
-	return &Vertex{Value: v}
+    return &Vertex{Value: v}
 }
 
 func (g *Graph) AddVertex(v int) {
-	g.Vertices = append(g.Vertices, NewVertex(v))
+    g.Vertices = append(g.Vertices, NewVertex(v))
 }
 
 func (g *Graph) findVertex(v int) *Vertex {
-	for _, vertex := range g.Vertices {
-		if vertex.Value == v {
-			return vertex
-		}
-	}
-	return nil			// tratamento de erro 10/10
+    for _, vertex := range g.Vertices {
+        if vertex.Value == v {
+            return vertex
+        }
+    }
+    return nil			// tratamento de erro 10/10
 }
 
 func (g *Graph) AddEdge(e1, e2 int) {
-	v1, v2 := g.findVertex(e1), g.findVertex(e2)
-
-	if v1 == nil || v2 == nil {
-		fmt.Println("deu ruim man. nao tem esse vertice ai nao"); return
-	} else {
-		v1.Adjacents = append(v1.Adjacents, v2)
-	}
+    v1, v2 := g.findVertex(e1), g.findVertex(e2)
+    if v1 == nil || v2 == nil {
+        fmt.Println("deu ruim man. nao tem esse vertice ai nao"); return
+    } else {
+        v1.Adjacents = append(v1.Adjacents, v2)
+    }
 }
 
 func (g *Graph) ExistsEdge(e1, e2 int) bool {
-	v1, v2 := g.findVertex(e1), g.findVertex(e2)
+    v1, v2 := g.findVertex(e1), g.findVertex(e2)
+    for i := range v1.Adjacents {
+        if v1.Adjacents[i] == v2 {
+            return true
+        }
+    }
+    return false
+}
 
-	for i := range v1.Adjacents {
-		if v1.Adjacents[i] == v2 {
-			return true
-		}
-	}
-	return false
+func (g *Graph) Size() int {
+    return len(g.Vertices)
 }
 
 func (g *Graph) ShowVerts() {
-	for _, vert := range g.Vertices {
-		fmt.Println(vert.Value)
-	}
+    for _, vert := range g.Vertices {
+        fmt.Println(vert.Value)
+    }
 }
 
--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	// "fmt"
-	. "github.com/luthierycosta/N-puzzle/graph"
+    // "fmt"
+    . "github.com/luthierycosta/N-puzzle/graph"
 )
 
 func main() {
-	//x := NewVertex(2)
-	//y := NewVertex(1)
-	gra := NewGraph(2,1,2)
-	gra.ShowVerts()
+    //x := NewVertex(2)
+    //y := NewVertex(1)
+    gra := NewGraph(2,1,2)
+    gra.ShowVerts()
 
-	// fmt.Println(gra)
+    // fmt.Println(gra)
 }
