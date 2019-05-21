f0c17024ed20c92cd4a26916ed0fd6a6de60b249
Luthiery Costa <luthierycosta2010@gmail.com>
2019-05-21 15:00:35 -0300

    Alterações na fila de prioridade

--- pq/pq.go
+++ pq/pq.go
@@ -10,33 +10,30 @@ func (i *Item) Priority() float64 {return i.F}
 // Fila de prioridade que ordena os itens de forma crescente (menor valor primeiro).
 type PriorityQueue []Item
 
-// Aloca e retorna uma nova fila de prioridade com os itens especificados (opcionais).
-func New(elements ...Item) *PriorityQueue {
-    pq := new(PriorityQueue)
-    for i := range elements {
-        pq.Push(elements[i])
-    }
-    return pq
-}
-
 // Retorna o tamanho atual da fila.
 func (pq *PriorityQueue) Len() int {
     return len(*pq)
 }
 
 // Insere um elemento na fila de prioridade.
-func (pq *PriorityQueue) Push(value Item) {
-    // insere-o no fim da fila
-    *pq = append(*pq, value)
-    // percorre a fila pra posicionar o elemento na ordem certa
-    for i := len(*pq)-1; i > 0 && value.Priority() < (*pq)[i-1].Priority(); i-- {
-        (*pq)[i-1], (*pq)[i] = (*pq)[i], (*pq)[i-1]
+func (pq *PriorityQueue) Push(values ...Item) {
+    for _, value := range values {
+        // insere-o no fim da fila
+        *pq = append(*pq, value)
+        // percorre a fila pra posicionar o elemento na ordem certa
+        for i := len(*pq)-1; i > 0 && value.Priority() < (*pq)[i-1].Priority(); i-- {
+            (*pq)[i-1], (*pq)[i] = (*pq)[i], (*pq)[i-1]
+        }
     }
 }
 
 // Retira o elemento com maior prioridade na fila
 func (pq *PriorityQueue) Pop() (result Item) {
-    result = (*pq)[0]
-    *pq = (*pq)[1:]
+    if pq.Len() == 0 {
+        result = Item{}                 // não tem como retornar nil
+    } else {
+        result = (*pq)[0]
+        *pq = (*pq)[1:]
+    }
     return
 }
--- pqtest.go
+++ pqtest.go
@@ -6,12 +6,17 @@ import (
 )
 
 func main() {
-    fila := pq.New()
-    fila.Push(pq.Item{0.3})
-    fila.Push(pq.Item{0.2})
-    fila.Push(pq.Item{0.45})
-    fila.Push(pq.Item{0.7})
-    fila.Push(pq.Item{0.4})
+    var fila pq.PriorityQueue
+    fila.Push(pq.Item{0.3},pq.Item{0.2}, pq.Item{0.45},pq.Item{0.7}, pq.Item{0.4})
 
     fmt.Println(fila)
+    a := fila.Pop()
+    fmt.Println(fila)
+    b := fila.Pop()
+    fmt.Println(fila)
+    fila.Push(pq.Item{0.1}, pq.Item{0.6})
+    c := fila.Pop()
+
+    fmt.Println(fila)
+    fmt.Println(a, b, c)
 }
