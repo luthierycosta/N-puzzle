d8a38b1c5cbaaff5478b7202dbc3335b116ce0b3
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-05-22 11:32:31 -0300

    adicionei um teste com a biblioteca de assert

--- pq/pq_test.go
+++ pq/pq_test.go
@@ -1,7 +1,11 @@
 package pq
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
+// sem usar a bliblioteca de asser
 func TestPush(t *testing.T) {
 	fila := PriorityQueue{}
 	item := Item{2.4}
@@ -9,4 +13,17 @@ func TestPush(t *testing.T) {
 	if fila.Len() != 1 {
 		t.Errorf("Deu erro no pq.Push()")
 	}
+}
+
+// usando a biblioteca de assert
+func TestPushMultiplos(t *testing.T) {
+	assert := assert.New(t)
+
+	fila := PriorityQueue{}
+	fila.Push(Item{2.5})
+	fila.Push(Item{2.6})
+	fila.Push(Item{2.7})
+	fila.Push(Item{2.8})
+
+	assert.Equal(fila.Len(), 4)
 }
