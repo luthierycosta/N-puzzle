87b60d630e53a3e5d4915c525a82a07cddda5a03
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-05-22 11:48:39 -0300

    teste final de pq adicionado. tudo ok

--- pq/pq_test.go
+++ pq/pq_test.go
@@ -27,3 +27,29 @@ func TestPushMultiplos(t *testing.T) {
 
 	assert.Equal(fila.Len(), 4)
 }
+
+func TestPop(t *testing.T) {
+	assert := assert.New(t)
+
+	fila := PriorityQueue{}
+	
+
+	fila.Push(Item{0.3},Item{0.2}, Item{0.45},Item{0.7}, Item{0.4})
+
+	a := fila.Pop()
+	b := fila.Pop()
+	fila.Push(Item{0.1}, Item{0.6})
+	c := fila.Pop()
+
+	assert.Equal(a, Item{0.2})
+	assert.Equal(b, Item{0.3})
+	assert.Equal(c, Item{0.1})
+}
+
+func TestPopOnEmpty(t *testing.T) {
+	assert := assert.New(t)
+
+	fila := PriorityQueue{}
+	
+	assert.Equal(fila.Pop(), Item{})
+}
