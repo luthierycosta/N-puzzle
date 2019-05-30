54811fe70e8ce60773bca8a47b3f399b278c587a
luthierycosta <luthierycosta2010@gmail.com>
2019-05-30 10:02:28 -0300

    Correções na função h

--- state/heuristics.go
+++ state/heuristics.go
@@ -5,7 +5,7 @@ package state
 func (a *State) DistanceTo(b *State) (res int) {
     n := len(a.gameBoard)
     // for every block i in the puzzle, compare its position on both boards and take the difference
-    for i := 0; i < n*n-1; i++ {
+    for i := 1; i < n*n; i++ {
         res += difference(a.findPos(i), b.findPos(i))
     }
     return
@@ -22,11 +22,14 @@ func (s *State) findPos(k int) Pair {
     return Pair{-1,-1}      // throw some error
 }
 
-func difference(a, b Pair) (res int) {
-    // below code is equivalent to: res = math.Abs(a.X-b.X) + math.Abs(a.Y-b.Y)
-    // Abs only supports floats
-    xdiff, ydiff := a.X-b.X, a.Y-b.Y
-    if xdiff < 0 { res += -xdiff } else { res += xdiff }
-    if ydiff < 0 { res += -ydiff } else { res += ydiff }
-    return
+func difference(a, b Pair) int {
+    return abs(a.X-b.X) + abs(a.Y-b.Y)
+}
+
+func abs(x int) int {
+    if x < 0 {
+        return -x
+    } else {
+        return x
+    }
 }
