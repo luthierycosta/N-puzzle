e71ce814112d7f20c116f4e71cab29bf3c9b1c1b
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2019-05-30 15:28:55 -0300

    Join path implementado e testado

--- path/path.go
+++ path/path.go
@@ -3,6 +3,13 @@ package path
 // Direction enum de direcao
 type Direction uint8
 
+func (d Direction) invert() Direction {
+	if (d < 2) {
+		return d + 2
+	}
+	return d - 2
+}
+
 // valores possiveis de Direction
 const (
 	Up Direction = 0
@@ -17,4 +24,13 @@ type Path []Direction
 // MakeMove funcao de fazer um movimento
 func (p *Path) MakeMove(dir Direction) Path {
 	return append(*p, dir)
+}
+
+func (p Path) Join(pathToJoin Path) Path {
+	pLen := len(pathToJoin)
+	for left, right := 0, pLen-1; left < right; left, right = left+1, right-1 {
+		pathToJoin[left], pathToJoin[right] = pathToJoin[right].invert(), pathToJoin[left].invert()
+	}
+
+	return append(p, pathToJoin...)
 }
--- path/path_test.go
+++ path/path_test.go
@@ -21,3 +21,23 @@ func TestMakeMove(t *testing.T) {
 	assert.Equal(path, Path{
 		Up, Up, Down, Down, Left, Right, Left, Right})
 }
+
+func TestJoinPath(t *testing.T) {
+	assert := assert.New(t)
+
+	path1 := Path{}
+	path1 = path1.MakeMove(Up)
+	path1 = path1.MakeMove(Down)
+	path1 = path1.MakeMove(Left)
+	
+	path2 := Path{}
+	path2 = path2.MakeMove(Up)
+	path2 = path2.MakeMove(Left)
+	path2 = path2.MakeMove(Down)
+	path2 = path2.MakeMove(Right)
+
+	joinedPath := path1.Join(path2)
+
+	assert.Equal(joinedPath, Path{
+		Up, Down, Left, Left, Up, Right, Down })
+}
